38aad902a17dfa8cb7ff980a3c5c78436630573f
rubycrafter <anton_freeman@inbox.ru>
2015-09-21 15:52:20 +0500

    Added goddamned 1251 encoding support

--- parser.rb
+++ parser.rb
@@ -1,8 +1,14 @@
+#encoding: UTF-8
 require 'open-uri'
 require 'nokogiri'
 
 catalog_url  = 'http://www.a-yabloko.ru/catalog/'
 catalog_html = open(catalog_url)
 catalog_doc  = Nokogiri::HTML(catalog_html)
+catalog_doc.encoding = 'UTF-8'
 
-puts catalog_doc
+groups = []
+
+catalog_doc.css('.children a').each do |group|
+  puts group
+end
