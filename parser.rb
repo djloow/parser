d1f3437e8f60d41254b0e99016948e70216bad88
rubycrafter <anton_freeman@inbox.ru>
2015-09-22 16:06:37 +0500

    Parsing of main page works

--- parser.rb
+++ parser.rb
@@ -3,32 +3,52 @@ require 'nokogiri'
 require 'digest'
 require 'mechanize'
 
-catalog_url  = 'http://www.a-yabloko.ru/catalog/' # кодировка страницы windows-1251
-catalog_html = open(catalog_url)
-catalog_doc  = Nokogiri::HTML(catalog_html)
-catalog_doc.encoding = 'UTF-8' # конвертируем 1251 to UTF-8
-agent = Mechanize.new
-page  = agent.get(catalog_url)
-page.encoding = 'UTF-8' # конвертируем 1251 to UTF-8
-puts page
 
-output_file = File.open("./catalog.txt", "w")
+class Parser
 
+  def initialize
 
-#def scan_main()
-  catalog_doc.css('.children a').each do |string|
-    type    = "Group"
-    group   = "-----"
-    name    = string.to_s.scan(%r{\)">(.*)<span>}m)[0][0] # названия главных категорий с главной страницы сайта
-    id      = Digest::MD5.hexdigest(type+group+name)
-    picture = string.to_s.scan(%r{thumbs/(.*)\)">}m)[0]
-    picture = picture ? picture[0] : '-----------------------------------------'
-    $,      = "\t"
+    @catalog_url  = 'http://www.a-yabloko.ru/catalog/' # кодировка страницы windows-1251
+    @catalog_html = open(@catalog_url)
+    @catalog_doc  = Nokogiri::HTML(@catalog_html)
+    @catalog_doc.encoding = 'UTF-8' # конвертируем 1251 to UTF-8
 
-    output_file.print id, type, group, picture, name
-    output_file.puts
+    @agent = Mechanize.new
+    @page  = @agent.get(@catalog_url)
+    puts @page
+
+    @@output_file = File.open("./catalog.txt", "w")
   end
-#end
 
+  def download(pic)
+    open('pictures/'+pic, 'wb') do |file|
+      file << open('http://www.a-yabloko.ru/storage/catalog/.thumbs/'+pic).read
+    end
+  end
 
-#scan_main()
+  # Метод составляет список категорий с главной страницы сайта и загружает соответствующие картинки
+  def scan_main
+    @catalog_doc.css('.children a').each do |string|
+      type    = "Group"
+      group   = "-----"
+      name    = string.to_s.scan(%r{\)">(.*)<span>}m)[0][0] # названия главных категорий с главной страницы сайта
+      id      = Digest::MD5.hexdigest(type+group+name)
+      picture = string.to_s.scan(%r{thumbs/(.*)\)">}m)[0]
+      download(picture[0]) unless picture.nil? # если картинка есть - скачиваем её
+      picture = picture ? picture[0] : '-----------------------------------------' # если картинки нет - ставим прочерк
+      $,      = "\t" # разделитель для print
+      @@output_file.print id, type, group, picture, name
+      @@output_file.puts
+    end
+  end
+
+  def scan_groups
+
+  end
+
+end
+
+parser = Parser.new
+parser.scan_main
+
+parser.scan_groups
