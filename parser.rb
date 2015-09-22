b47892af7e31cc26418caaccffa6c3653a6d4850
rubycrafter <anton_freeman@inbox.ru>
2015-09-22 12:13:11 +0500

    Some good code with Nokogiri only

--- parser.rb
+++ parser.rb
@@ -1,14 +1,34 @@
-#encoding: UTF-8
 require 'open-uri'
 require 'nokogiri'
+require 'digest'
+require 'mechanize'
 
-catalog_url  = 'http://www.a-yabloko.ru/catalog/'
+catalog_url  = 'http://www.a-yabloko.ru/catalog/' # кодировка страницы windows-1251
 catalog_html = open(catalog_url)
 catalog_doc  = Nokogiri::HTML(catalog_html)
-catalog_doc.encoding = 'UTF-8'
+catalog_doc.encoding = 'UTF-8' # конвертируем 1251 to UTF-8
+agent = Mechanize.new
+page  = agent.get(catalog_url)
+page.encoding = 'UTF-8' # конвертируем 1251 to UTF-8
+puts page
 
-groups = []
+output_file = File.open("./catalog.txt", "w")
 
-catalog_doc.css('.children a').each do |group|
-  puts group
-end
+
+#def scan_main()
+  catalog_doc.css('.children a').each do |string|
+    type    = "Group"
+    group   = "-----"
+    name    = string.to_s.scan(%r{\)">(.*)<span>}m)[0][0] # названия главных категорий с главной страницы сайта
+    id      = Digest::MD5.hexdigest(type+group+name)
+    picture = string.to_s.scan(%r{thumbs/(.*)\)">}m)[0]
+    picture = picture ? picture[0] : '-----------------------------------------'
+    $,      = "\t"
+
+    output_file.print id, type, group, picture, name
+    output_file.puts
+  end
+#end
+
+
+#scan_main()
