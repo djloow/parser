8b5a6676a61564628a58a2737a88614c39add67d
rubycrafter <anton_freeman@inbox.ru>
2015-09-23 13:20:17 +0500

    Scanning groups works fine. Need to find the way to add group.

--- parser.rb
+++ parser.rb
@@ -3,17 +3,17 @@ require 'nokogiri'
 require 'digest'
 require 'curl'
 
-
 class Parser
 
-  @@output_file = File.open("./catalog.txt", "w")
+  @@output_file ||= File.open("./catalog.txt", "w")
+  @@depth = 0
+  $,      = "\t" # разделитель для print
 
   def initialize(url)
-    @catalog_url  = url # кодировка страницы windows-1251
-    @catalog_html = Curl.get(@catalog_url).body_str
+    @catalog_html = Curl.get(url).body_str
     @catalog_doc  = Nokogiri::HTML(@catalog_html)
     @catalog_doc.encoding = 'UTF-8' # конвертируем 1251 to UTF-8
-    puts @catalog_doc
+    #puts @catalog_html
   end
 
   # Все картинки на сервере лежат в одной папке,
@@ -26,26 +26,44 @@ class Parser
 
   # Метод составляет список категорий с главной страницы сайта и загружает соответствующие картинки
   def scan_groups
-    #puts @catalog_doc.css('.children a')[0]
-    @catalog_doc.css('.children a').each do |string|
-      type    = "Group"
+    puts "-Scanning groups..."
+    @@depth += 1
+    puts "***Depth is #{@@depth}"
+    links = scan_footer if @@depth == 1
+    strings = @catalog_doc.css('.children a')
+    strings.each do |string|
+      type    = "sub-"*(@@depth-1)+"Group"
       group   = "-----"
       name    = string.to_s.scan(%r{\)">(.*)<span>}m)[0][0] # названия главных категорий с главной страницы сайта
+      puts name
       id      = Digest::MD5.hexdigest(type+group+name)
       picture = string.to_s.scan(%r{thumbs/(.*)\)">}m)[0]
-      download(picture[0]) unless picture.nil? # если картинка есть - скачиваем её
+      #download(picture[0]) unless picture.nil? # если картинка есть - скачиваем её
       picture = picture ? picture[0] : '-----------------------------------------' # если картинки нет - ставим прочерк
-      $,      = "\t" # разделитель для print
-      @@output_file.print id, type, group, picture, name
-      @@output_file.puts
-      link = string.to_s.scan(%r{href="(.*)" style})
-      puts link
-      #parser = Parser.new(link)
+      @@output_file.print id, type, group, picture, name+"\n"
+      if @@depth == 1
+        parser = Parser.new('http://www.a-yabloko.ru'+links.shift)
+      else
+        parser = Parser.new('http://www.a-yabloko.ru'+string.to_s.scan(%r{href="(.*)" })[0][0])
+      end
+      parser.scan_groups
     end
+    scan_goods if strings.empty?
+
+    @@depth -= 1
+  end
+
+  def scan_footer
+    puts "--Scanning footer..."
+    links = Array.new
+    @catalog_doc.css('a.root').each do |string|
+      links << string.to_s.scan(%r{href="(.*)">})[0][0]
+    end
+    links
   end
 
   def scan_goods
-
+    puts "---Scanning goods....."
   end
 
 end
