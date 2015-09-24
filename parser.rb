a947be487b58d285cd48bb1f07722927eb2b311d
rubycrafter <anton_freeman@inbox.ru>
2015-09-24 16:34:11 +0500

    Little fixes

--- parser.rb
+++ parser.rb
@@ -1,16 +1,20 @@
 require 'open-uri'
+require 'csv'
 require 'nokogiri'
 require 'digest'
 require 'curl'
+require 'csv'
 
 class Parser
 
-  @@output_file ||= File.open("./catalog.txt", "w")
-  @@depth = 0    # глубина рекурсии
-  $,      = "\t" # разделитель для print
-  @@total = 0    # общее количество записей
+  @@headers = %w{id, type, group, pic, name}
+  puts @@headers.inspect
+  @@catalog  ||= CSV.open("./catalog.txt", "w", col_sep: "\t", encoding: 'UTF-8', headers: @@headers)
+  @@depth          = 0    # глубина рекурсии
+  @@col_sep        = "\t" # разделитель для print
+  @@total          = 0    # общее количество записей
   @@total_in_group = Hash.new
-  @@current_group = ""
+  @@current_group  = ""
 
   def initialize(url)
     @catalog_html = Curl.get(url).body_str # скачиваем страницу, она в windows-1251
@@ -37,40 +41,50 @@ class Parser
     puts "-Scanning groups..."
     @@depth += 1
     puts "***Depth is #{@@depth}"
-    links = scan_footer if @@depth == 1
-    if @@depth > 1
-      group = @catalog_doc.css('#content.bar h1').text
-    else
-      group = "---------"
-    end
-    strings = @catalog_doc.css('.children a')
-    strings.each do |string|
+    group = @catalog_doc.css('#content.bar h1').text
+    rows = @catalog_doc.css('.children a')
+    rows.each do |row|
       type    = "sub-"*(@@depth-1)+"group"
-      name    = string.to_s.scan(%r{\)">(.*)<span>}m)[0][0] # названия главных категорий с главной страницы сайта
+      name    = row.to_s.scan(%r{\)">(.*)<span>}m)[0][0] # названия главных категорий с главной страницы сайта
       puts name
-      @@current_group = name if @@depth == 1
       id      = Digest::MD5.hexdigest(type+group+name)
-      picture = string.to_s.scan(%r{thumbs/(.*)\)">}m)[0]
+      picture = row.to_s.scan(%r{thumbs/(.*)\)">}m)[0]
       download_gr(picture[0]) unless picture.nil? # если картинка есть - скачиваем её
       picture = picture ? picture[0] : '-----------------------------------------' # если картинки нет - ставим прочерк
-      if @@depth == 1
-        parser = Parser.new('http://www.a-yabloko.ru'+links.shift)
-      else
-        parser = Parser.new('http://www.a-yabloko.ru'+string['href'])  #.to_s.scan(%r{href="(.*)" })[0][0])
-      end
+      parser = Parser.new('http://www.a-yabloko.ru'+row['href'])
       add_record(id, type, group, picture, name)
       parser.scan_groups
     end
-    scan_goods if strings.empty?
+    scan_goods if rows.empty?
 
     @@depth -= 1
+  end
+
+  def scan_main
+    puts "Scanning main page..."
+    links = scan_footer
+    group = "---------"
+    rows = @catalog_doc.css('.children a')
+    rows.each do |row|
+      type    = "group"
+      name    = row.to_s.scan(%r{\)">(.*)<span>}m)[0][0] # названия главных категорий с главной страницы сайта
+      puts name
+      @@current_group = name
+      id      = Digest::MD5.hexdigest(type+group+name)
+      picture = row.to_s.scan(%r{thumbs/(.*)\)">}m)[0]
+      download_gr(picture[0]) unless picture.nil? # если картинка есть - скачиваем её
+      picture = picture ? picture[0] : '-----------------------------------------' # если картинки нет - ставим прочерк
+      parser = Parser.new('http://www.a-yabloko.ru'+links.shift)
+      add_record(id, type, group, picture, name)
+      parser.scan_groups
+    end
   end
 
   def scan_footer
     puts "--Scanning footer..."
     links = Array.new
-    @catalog_doc.css('a.root').each do |string|
-      links << string['href']  #.to_s.scan(%r{href="(.*)">})[0][0]
+    @catalog_doc.css('a.root').each do |row|
+      links << row['href']
     end
     links
   end
@@ -78,13 +92,13 @@ class Parser
   def scan_goods
     puts "---Scanning goods....."
     group = @catalog_doc.css('#content.bar h1').text
-    strings = @catalog_doc.css('div.goods .img')
-    strings.each do |string|
-      puts string['title']
+    rows = @catalog_doc.css('div.goods .img')
+    rows.each do |row|
+      #puts row['title']
       type = "Item"
-      name = string['title']
+      name = row['title']
       id = Digest::MD5.hexdigest(type+group+name)
-      picture = string.to_s.scan(%r{thumbs/(.*)'\)" }m)[0]
+      picture = row.to_s.scan(%r{thumbs/(.*)'\)" }m)[0]
       if !picture || picture[0] == 'no_img_w280h140.png'
         picture = '-----------------------------------------'
       else
@@ -96,13 +110,13 @@ class Parser
   end
 
   def add_record(id, type, group, picture, name)
-    @@output_file.print id, type, group, picture, name+"\n"
+    @@catalog << [id, type, group, picture, name]
+    #puts CSV::Table(@@catalog)['name']
     @@total += 1
-    
   end
 
   def start
-    scan_groups
+    scan_main
   end
 
 end
