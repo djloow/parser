c452447d6be812c456850c89059ca9534d7323f4
rubycrafter <you@example.com>
2015-09-27 16:17:55 +0500

    Починил загрузку картинок товаров, добавил подсчёт количества товара в группах.

--- parser.rb
+++ parser.rb
@@ -6,17 +6,17 @@ require 'curl'
 
 class Parser
 
-  @@headers = %w{id, type, group, pic, name}
-  @@catalog  ||= CSV.read("catalog.txt", "a+", col_sep: "\t", headers: true, converters: :numeric, :header_converters => :symbol).map { |row| row.to_h }
-  @@depth          = 0    # глубина рекурсии
-  @@col_sep        = "\t" # разделитель для print
-  @@total          = 0    # общее количество записей
-  @@total_in_group = Hash.new(0)
-  @@current_group  = ""
-  @@start          = Time.now
+  @@headers = %w{type, group, pic, name}
+  @@catalog  ||= CSV.read("catalog.txt", "a+", col_sep: "\t", headers: true, converters: :numeric, header_converters: :symbol).map { |row| row.to_h }
+  @@depth          ||= 0    # глубина рекурсии
+  @@col_sep        ||= "\t" # разделитель для print
+  @@total          ||= 0    # общее количество записей
+  @@total_in_group ||= Hash.new(0)
+  @@current_group  ||= ""
+  @@start          ||= Time.now
 
   def initialize(url)
-    @catalog_html = Curl.get(url).body_str # скачиваем страницу, она в windows-1251
+    @catalog_html = open(url, "Cookie" => "pgs=500") # скачиваем страницу, она в windows-1251
     @catalog_doc  = Nokogiri::HTML(@catalog_html) # создаём документ
     @catalog_doc.encoding = 'UTF-8' # конвертируем 1251 to UTF-8
   end
@@ -47,15 +47,15 @@ class Parser
       type    = "sub-"*(@@depth-1)+"group"
       name    = row.to_s.scan(%r{\)">(.*)<span>}m)[0][0] # названия главных категорий с главной страницы сайта
       puts name
-      id      = Digest::MD5.hexdigest(type+group+name)
+      #id      = Digest::MD5.hexdigest(type+group+name)
       picture = row.to_s.scan(%r{thumbs/(.*)\)">}m)[0]
-      download_group(picture[0]) unless picture.nil? # если картинка есть - скачиваем её
+      #download_group(picture[0]) unless picture.nil? # если картинка есть - скачиваем её
       picture = picture ? picture[0] : '-----------------------------------------' # если картинки нет - ставим прочерк
       parser = Parser.new('http://www.a-yabloko.ru'+row['href'])
-      add_record([id, type, group, picture, name])
+      add_record([type, group, picture, name])
       parser.scan_groups
     end
-    #scan_goods if rows.empty?
+    scan_goods if rows.empty?
 
     @@depth -= 1
   end
@@ -70,12 +70,12 @@ class Parser
       name    = row.to_s.scan(%r{\)">(.*)<span>}m)[0][0] # названия главных категорий с главной страницы сайта
       puts name
       @@current_group = name
-      id      = Digest::MD5.hexdigest(type+group+name)
+      #id      = Digest::MD5.hexdigest(type+group+name)
       picture = row.to_s.scan(%r{thumbs/(.*)\)">}m)[0]
       download_group(picture[0]) unless picture.nil? # если картинка есть - скачиваем её
       picture = picture ? picture[0] : '-----------------------------------------' # если картинки нет - ставим прочерк
       parser = Parser.new('http://www.a-yabloko.ru'+links.shift)
-      add_record([id, type, group, picture, name])
+      add_record([type, group, picture, name])
       parser.scan_groups
     end
   end
@@ -96,21 +96,28 @@ class Parser
     rows.each do |row|
       type = "Item"
       name = row['title']
-      id = Digest::MD5.hexdigest(type+group+name)
+      #id = Digest::MD5.hexdigest(type+group+name)
       picture = row.to_s.scan(%r{thumbs/(.*)'\)" }m)[0]
-      if !picture || picture[0] == 'no_img_w280h140.png'
+      if picture.nil? || picture[0] == 'no_img_w280h140.png'
         picture = '-----------------------------------------'
       else
         picture = picture[0]  # если картинки нет - ставим прочерк
-        download_item(picture[0]) # если картинка есть - скачиваем её
+        download_item(picture) # если картинка есть - скачиваем её
       end
-      add_record([id, type, group, picture, name])
+      add_record([type, group, picture, name])
+      @@total += 1
+      puts @@total
+      @@total_in_group[@@current_group] += 1
     end
   end
 
   def add_record(arr)
     @@catalog << arr
-    @@total += 1
+    print_stat if @@total == 1000
+  end
+
+  def print_stat 
+    puts @@total_in_group.inspect
   end
 
   def save
@@ -127,6 +134,7 @@ class Parser
     scan_main
     save
     puts Time.now - @@start
+    puts @@total
   end
 
 end
