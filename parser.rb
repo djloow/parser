ef6f96d47a8fa9e1b5c3b8faa7899253510cc24f
rubycrafter <anton_freeman@inbox.ru>
2015-10-09 14:19:59 +0500

    Nothing works now

--- parser.rb
+++ parser.rb
@@ -2,63 +2,65 @@ require 'open-uri'
 require 'csv'
 require 'nokogiri'
 require 'digest'
-require 'ruby-progressbar'
+require 'mechanize'
 
 class Parser
 
-  @@headers        ||= %w{type, group, pic, name}
-  if File.exist?("catalog.txt")
-    @@catalog      ||= CSV.read("catalog.txt", "r", col_sep: "\t", headers: false, converters: :numeric, header_converters: :symbol).map { |row| row.to_a } 
-  else
-    @@catalog      ||= CSV.read("catalog.txt", "w+") 
+  def open_catalog
+    if File.exist?("catalog.txt")
+      CSV.read("catalog.txt", "r", col_sep: "\t",
+              headers: false,
+              converters: :numeric,
+              header_converters: :symbol).
+              map { |row| row.to_a }
+    else
+      CSV.read("catalog.txt", "w+") 
+    end
   end
-  @@depth          ||= 0    # глубина рекурсии
-  @@col_sep        ||= "\t" # разделитель для print
-  @@total          ||= 0    # общее количество записей
+
+  @@headers        ||= %w{type, group, pic, name}
+  @@depth          ||= 0
+  @@col_sep        ||= "\t"
+
+  @@total          ||= 0
   @@total_in_group ||= Hash.new(0)
   @@current_group  ||= ""
-  @@start          ||= Time.now
   @@wo_pic         ||= 0
   @@pic_size       ||= Hash.new(0)
   @@total_size     ||= 0
-  @@progressbar    ||= ProgressBar.create(total: 10850, format: "%t: |%B| %p%% complete. ")
+
 
   def initialize(url)
-    @catalog_html = open(url, "Cookie" => "pgs=500") # скачиваем страницу, она в windows-1251
-    @catalog_doc  = Nokogiri::HTML(@catalog_html) # создаём документ
-    @catalog_doc.encoding = 'UTF-8' # конвертируем 1251 to UTF-8
+    @@catalog ||= open_catalog
+    @catalog_doc = Mechanize.new.get(url, "Cookie" => "pgs=500")
+    @catalog_doc.encoding = 'windows-1251'
   end
 
-  # Все картинки на сервере лежат в одной папке,
-  # а значит имеют уникальные имена, их и возьмём.
-  def download_group(pic)
+  def download_pic(pic)
     open('pictures/'+pic, 'wb') do |file|
       file << open('http://www.a-yabloko.ru/storage/catalog/.thumbs/'+pic).read
     end
   end
 
-  # Ладно, не в одной, а в двух папках.
   def download_item(pic)
     open('pictures/'+pic, 'wb') do |file|
       file << open('http://www.a-yabloko.ru/storage/catalog/goods/.thumbs/'+pic).read
     end
   end
 
-  # Метод составляет список категорий с главной страницы сайта и загружает соответствующие картинки
   def scan_groups
     #puts "-Scanning groups..."
     @@depth += 1
     #puts "***Depth is #{@@depth}"
-    group = @catalog_doc.css('#content.bar h1').text
-    rows = @catalog_doc.css('.children a')
+    group = @catalog_doc.search('#content.bar h1').text
+    rows = @catalog_doc.search('.children a')
     rows.each do |row|
       type    = "sub-"*(@@depth-1)+"group"
       name    = row.to_s.scan(%r{\)">(.*)<span>}m)[0][0] # названия главных категорий с главной страницы сайта
       #puts name
       #id      = Digest::MD5.hexdigest(type+group+name)
       picture = row.to_s.scan(%r{thumbs/(.*)\)">}m)[0]
-      #download_group(picture[0]) unless picture.nil? # если картинка есть - скачиваем её
-      picture = picture ? picture[0] : '-----------------------------------------' # если картинки нет - ставим прочерк
+      picture = picture ? picture[0] : '-'
       parser = Parser.new('http://www.a-yabloko.ru'+row['href'])
       add_record([type, group, picture, name])
       parser.scan_groups
@@ -72,7 +74,7 @@ class Parser
     #puts "Scanning main page..."
     links = scan_footer
     group = "---------"
-    rows = @catalog_doc.css('.children a')
+    rows = @catalog_doc.search('.children a')
     rows.each do |row|
       type    = "group"
       name    = row.to_s.scan(%r{\)">(.*)<span>}m)[0][0] # названия главных категорий с главной страницы сайта
@@ -80,8 +82,8 @@ class Parser
       @@current_group = name
       #id      = Digest::MD5.hexdigest(type+group+name)
       picture = row.to_s.scan(%r{thumbs/(.*)\)">}m)[0]
-      download_group(picture[0]) unless picture.nil? # если картинка есть - скачиваем её
-      picture = picture ? picture[0] : '-----------------------------------------' # если картинки нет - ставим прочерк
+      download_pic(picture[0]) unless picture.nil? # если картинка есть - скачиваем её
+      picture = picture ? picture[0] : '-' # если картинки нет - ставим прочерк
       parser = Parser.new('http://www.a-yabloko.ru'+links.shift)
       add_record([type, group, picture, name])
       parser.scan_groups
@@ -103,7 +105,7 @@ class Parser
   def scan_footer
     #puts "--Scanning footer..."
     links = Array.new
-    @catalog_doc.css('a.root').each do |row|
+    @catalog_doc.search('a.root').each do |row|
       links << row['href']
     end
     bad_links = ["/catalog/340/", "/catalog/343/"]
@@ -113,29 +115,25 @@ class Parser
 
   def scan_goods
     #puts "---Scanning goods....."
-    group = @catalog_doc.css('#content.bar h1').text
-    rows = @catalog_doc.css('div.goods .img')
+    group = @catalog_doc.search('#content.bar h1').text
+    rows = @catalog_doc.search('div.goods .img')
     rows.each do |row|
       type = "Item"
       name = row['title']
       #id = Digest::MD5.hexdigest(type+group+name)
       picture = row.to_s.scan(%r{thumbs/(.*)'\)" }m)[0]
       if picture.nil? || picture[0] == 'no_img_w280h140.png'
-        # если картинки нет - ставим прочерк
-        picture = '-----------------------------------------'
+        picture = '-'
         @@wo_pic += 1
       else
         picture = picture[0]
-        download_item(picture) # если картинка есть - скачиваем её
-        # Картинка ещё не скачалась, но файл уже занимает её
-        # точный размер.
+        download_pic(picture)
         @@pic_size[picture] = File.size("pictures/"+picture)
         @@total_size += @@pic_size[picture]
       end
       add_record([type, group, picture, name])
       @@total += 1
       @@total_in_group[@@current_group] += 1
-      @@progressbar.increment
     end
   end
 
@@ -149,22 +147,27 @@ class Parser
   def print_stat 
     puts "******************Summary by first 1000 goods******************"
     @@total_in_group.each do |group, count|
-      pc = 100*count/@@total.to_f
+      pc = (count/@@total.to_f)*100
       puts "#{group}: #{count} items, #{pc}% of total"
     end
+
     puts "Percent goods with pictures: " + (100*(@@total - @@wo_pic) / (@@total.to_f)).round(1).to_s + "%"
+
     top_size = @@pic_size.max_by { |pic, size| size }
     puts "Top size image: " + (top_size[0]).to_s + "; size: " + ((top_size[1].to_f)/1024).round(1).to_s + " kB"
+
     least_size = @@pic_size.min_by { |pic, size| size }
     puts "Least size image: " + (least_size[0]).to_s + "; size: " + ((least_size[1].to_f)/1024).round(1).to_s + " kB"
+
     average_size = (@@total_size.to_f)/1000/1024
     puts "Average image size: " + average_size.round(1).to_s + " kB"
+    save
   end
 
   def save
     @@catalog.uniq!
     open("tmp.txt", "w")  { |file| file.puts @@catalog.inspect } # DBG 
-    CSV.open("catalog.txt", "w", col_sep: "\t", encoding: 'UTF-8', headers: true, converters: :numeric, header_converters: :symbol) do |cat|
+    CSV.open("catalog.txt", "w", col_sep: "\t", encoding: 'windows-1251', headers: true, converters: :numeric, header_converters: :symbol) do |cat|
       @@catalog.each do |row|
         cat << row
       end
@@ -175,7 +178,6 @@ class Parser
     add_record(@@headers)
     scan_main
     save
-    puts "Time spent: ", Time.now - @@start
     puts "Total goods: ", @@total
   end
 
